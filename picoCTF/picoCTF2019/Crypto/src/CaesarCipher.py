0757f38ba30fc87180da5092093675eefc05c0ff
Takuya Iwase <tanu.z0uz0u@gmail.com>
2021-01-11 14:08:31 +0900

    [picoCTF][Crypto]CaesarCipher.py修正

--- picoCTF/picoCTF2019/Crypto/src/CaesarCipher.py
+++ picoCTF/picoCTF2019/Crypto/src/CaesarCipher.py
@@ -3,19 +3,21 @@ import re
 import sys
 
 cipher = sys.argv[1]
-symbo = '{}_'
+# cipher = 'Ercy{ABC_abc}'
 gap = range(0, 25)
 for num in gap:
     outstr = ''
     for i in range(len(cipher)):
-        if re.search(cipher[i], symbo):
+        # if re.search(cipher[i], symbo):
+            # outstr = outstr + cipher[i]
+        if ord(cipher[i]) < 65 or ord(cipher[i]) > 122:  # symbolic character
             outstr = outstr + cipher[i]
-        elif ord(cipher[i]) == 32:  # white space
+        elif ord(cipher[i]) > 90 and ord(cipher[i]) < 97:  # symbolic character
             outstr = outstr + cipher[i]
-        elif ord(cipher[i]) < 65 + num: # capital letter
+        elif ord(cipher[i]) < 65 + num: # capital character
             asc = ord(cipher[i]) + 26 - num
-            outstr = outstr + chr(asc)
-        elif ord(cipher[i]) < 97 + num and ord(cipher[i]) >= 97: # small letter
+            outstr = outstr + chr(asc)        
+        elif ord(cipher[i]) < 97 + num and ord(cipher[i]) > 96: # small character
             asc = ord(cipher[i]) + 26 - num
             outstr = outstr + chr(asc)
         else:
@@ -24,4 +26,4 @@ for num in gap:
 
     print("GAP =", num, ":")
     print(outstr)
-    
+

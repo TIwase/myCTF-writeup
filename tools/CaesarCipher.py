2684fd0fb35a498da54867e81437b5e8a2cb34c1
Takuya Iwase <tanu.z0uz0u@gmail.com>
2021-04-06 00:19:03 +0900

    [tools] CaesarCipher.py修正

--- tools/CaesarCipher.py
+++ tools/CaesarCipher.py
@@ -2,14 +2,11 @@
 import re
 import sys
 
-# cipher = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_uJdSftmh}"
 cipher = input("Insert cipher text : ")
-gap = range(0, 25)
+gap = range(0, 26)
 for num in gap:
     outstr = ''
     for i in range(len(cipher)):
-        # if re.search(cipher[i], symbo):
-            # outstr = outstr + cipher[i]
         if ord(cipher[i]) < 65 or ord(cipher[i]) > 122:  # symbolic character
             outstr = outstr + cipher[i]
         elif ord(cipher[i]) > 90 and ord(cipher[i]) < 97:  # symbolic character
@@ -24,6 +21,5 @@ for num in gap:
             asc = ord(cipher[i]) - num
             outstr = outstr + chr(asc)
 
-    print("GAP =", num, ":")
-    print(outstr)
+    print("GAP =", num, ": ", outstr)
 

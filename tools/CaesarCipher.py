45378c32d950fb5b14ffbc151c4efae32edd3864
Takuya Iwase <tanu.z0uz0u@gmail.com>
2021-04-06 11:11:08 +0900

    [tools] CaesarCipher.py コード簡略化

--- tools/CaesarCipher.py
+++ tools/CaesarCipher.py
@@ -8,18 +8,18 @@ for num in gap:
     outstr = ''
     for i in range(len(cipher)):
         if ord(cipher[i]) < 65 or ord(cipher[i]) > 122:  # symbolic character
-            outstr = outstr + cipher[i]
+            outstr += cipher[i]
         elif ord(cipher[i]) > 90 and ord(cipher[i]) < 97:  # symbolic character
-            outstr = outstr + cipher[i]
+            outstr += cipher[i]
         elif ord(cipher[i]) < 65 + num: # capital character
             asc = ord(cipher[i]) + 26 - num
-            outstr = outstr + chr(asc)        
+            outstr += chr(asc)        
         elif ord(cipher[i]) < 97 + num and ord(cipher[i]) > 96: # small character
             asc = ord(cipher[i]) + 26 - num
-            outstr = outstr + chr(asc)
+            outstr += chr(asc)
         else:
             asc = ord(cipher[i]) - num
-            outstr = outstr + chr(asc)
+            outstr += chr(asc)
 
     print("GAP =", num, ": ", outstr)
 
